df99bd718d5111ca433859bb0a741dc1750156d9
meetd <bikash081995@gmail.com>
2024-06-02 12:58:54 +0530

    initial commit

---
+++ src/test/java/com/gfg/ewallet69/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfg.ewallet69.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
